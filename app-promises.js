0a9c703f8d9ccc06c976c7004b55756f8a227ae1
Raj Vasikarla <raj_vasikarla@intuit.com>
2018-10-15 13:29:16 -0700

    automatic commit message

--- app-promises.js
+++ app-promises.js
@@ -1,29 +1,81 @@
 const users = [{
     id: 1,
-    name: 'Raj',
+    name: 'Andrew',
     schoolId: 101
-}, {
+  }, {
     id: 2,
-    name: 'Sasha',
-    schoolId: 201
-}];
-const grades = [];
-
-const getUser = (id) => {
+    name: 'Jessica',
+    schoolId: 999
+  }];
+  
+  const grades = [{
+    id: 1,
+    schoolId: 101,
+    grade: 86
+  }, {
+    id: 2,
+    schoolId: 999,
+    grade: 100
+  }, {
+    id: 3,
+    schoolId: 101,
+    grade: 80
+  }];
+  
+  const getUser =  (id) => {
     return new Promise((resolve, reject) => {
-        console.log("Finding User...");
-        const user = users.find((user) => user.id === id); 
-        if(user){
-            console.log("Able to find user");
-            resolve(user);
-        }else{
-            reject(`Unable to find the user, with id of : ${id}`);
-        }
+      const user = users.find((user) => user.id === id);
+  
+      if (user) {
+        resolve(user);
+      } else {
+        reject(`Unable to find user with id of ${id}.`);
+      }
     });
-};
+  };
+  
+  const getGrades = (schoolId) => {
+    return new Promise((resolve, reject) => {
+      resolve(grades.filter((grade) => grade.schoolId === schoolId));
+    });
+  };
+  
+//   // Andrew has a 83% in the class
+//   const getStatus = (userId) => {
+//     let user;
+//     return getUser(userId).then((tempUser) => {
+//       user = tempUser;
+//       return getGrades(user.schoolId);
+//     }).then((grades) => {
+//       let average = 0;
+  
+//       if (grades.length > 0) {
+//         average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+//       }
+  
+//       return `${user.name} has a ${average}% in the class.`;
+//     });
+//   };
+  
+//   getStatus(2).then((status) => {
+//     console.log(status);
+//   }).catch((e) => {
+//     console.log(e);
+//   });
 
-getUser(2).then((user) => {
-    return(user);
-}).then((e) => {
-    console.log("Error");
+  // Andrew has a 83% in the class
+    const getStatusAlt = async (userId) => {
+        const user = await getUser(userId);
+        const grades = await getGrades(user.schoolId);
+
+      let average = 0;
+      if (grades.length > 0) {
+        average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+      }
+
+      return `${user.name} has a ${average}% in the class.`;
+    };
+
+getStatusAlt(1).then((name) => {
+    console.log(name);
 });
